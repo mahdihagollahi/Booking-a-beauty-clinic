e1870022c3453446500857d3c04693f86e492d23
mahdihagollahi <mahdihaghollahi@gmail.com>
2024-09-13 12:20:24 +0330

    add photo in reservation landing and fix deualt background

--- src/Component/Landing/ReservationServicePart.tsx
+++ src/Component/Landing/ReservationServicePart.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import Background from "@/assent/Img/Landing/background.svg";
 import Image from "next/image";
 import Flower from "@/assent/Img/Landing/flower.svg";
-// import WomanImage from "@/assent/Img/Landing/WomanReservation.svg";
+import WomanImage from "@/assent/Img/Landing/WomanResevation.svg";
 function ReservationServicePart() {
   return (
     <div>
       <div
-        className="absolute -z-1 inset-0 w-[100%] h-[50%] py-10 mt-1  "
+        className="absolute -z-1 inset-0 w-[100%] h-[54%] py-10 mt-1  "
         style={{ backgroundImage: `url(${Background.src})` }}
       >
         <div className=" mr-[80.9%] mt-2">
@@ -17,22 +17,24 @@ function ReservationServicePart() {
         <div className=" mr-[63.8%] mt-[110px]">
           <Image src={Flower} width={52} height={58} alt="" />
         </div>
-        {/* <div className="mr-[65%] -mt-60">
+        <div className="mr-[72.6%] -mt-[11%] absolute">
           <Image
                 
                 src={WomanImage}
-                width={550}
-                height={554}
+                width={290}
+                height={250}
                 alt=''
                 />
-        </div> */}
+        </div>
 
         <div>
-          <h1 className="font-demiBold text-[48px] -mt-[6%] mr-[5%] leading-6">
-            خدمات ما راز {""}
-            <span className="text-[#DC1E7A]">زیبایی</span>
-            {""} شماست
-          </h1>
+          <div>
+            <h1 className="font-demiBold text-[48px] -mt-[8%] mr-[5%]  leading-6">
+              خدمات ما راز {""}
+              <span className="text-[#DC1E7A]">زیبایی</span>
+              {""} شماست
+            </h1>
+          </div>
         </div>
 
         <div className="mt-16 mr-[5%] text-[#A5A5A5] font-regular text-xl">
