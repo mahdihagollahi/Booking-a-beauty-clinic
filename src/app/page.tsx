ed45e528bd254dfa87308ac9a8f12d31269c6288
mahdihagollahi <mahdihaghollahi@gmail.com>
2024-09-26 01:01:49 +0330

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,32 @@
-// import AboutMe from '@/Component/Landing/AboutMe'
-// import Footer from '@/Component/Landing/Footer'
-// import Navbar from '@/Component/Landing/Navbar'
-// import ReservationServicePart from '@/Component/Landing/ReservationServicePart'
-// import Services from '@/Component/Landing/Services'
-// import WhyMe from '@/Component/Landing/WhyMe'
-// import  BeautyService  from '@/Component/Landing/‌BeautyService'
-// import Doctor from '@/Component/Landing/Doctor'
-// import ContactWithCustomer from '@/Component/Landing/ContactWithCustomer'
+import AboutMe from '@/Component/Landing/AboutMe'
+import Footer from '@/Component/Landing/Footer'
+import Navbar from '@/Component/Landing/Navbar'
+import ReservationServicePart from '@/Component/Landing/ReservationServicePart'
+import Services from '@/Component/Landing/Services'
+import WhyMe from '@/Component/Landing/WhyMe'
+import  BeautyService  from '@/Component/Landing/‌BeautyService'
+import Doctor from '@/Component/Landing/Doctor'
+import ContactWithCustomer from '@/Component/Landing/ContactWithCustomer'
 
-import SingIn from '@/Component/Login/SingIn'
-import SingIn2 from '@/Component/Login/SingIn2'
-import LogIn1 from '@/Component/LogIn/LogIn1'
-import SingIn3 from '@/Component/SingIn/SingIn3'
-import AboutUsDiscription from "@/Component/AboutUs/AboutUsDiscription";
-import DetailArticle from "@/Component/Article/DetailArticle";
-import Doctor from "@/Component/Doctors/Doctors";
-import FishPeyment from "@/Component/Reservation/FishPeyment";
-import Information from "@/Component/Reservation/Information";
-import Payment from "@/Component/Reservation/Payment";
-import SelectService from "@/Component/Reservation/SelectService";
-import Service from "@/Component/Reservation/Service";
-import SetTimeAndDate from "@/Component/Reservation/SetTimeAndDate";
-import ShowService from "@/Component/Service/ShowService";
-import ChangePasswordModal from "@/Component/UserDashboard/component/ChangePasswordModal";
-import MyTurnComponent from "@/Component/UserDashboard/component/MyTurnComponent";
-import MyComment from "@/Component/UserDashboard/MyComment";
-import MyTurn from "@/Component/UserDashboard/MyTurn";
-import Profile from "@/Component/UserDashboard/Profile";
+// import SingIn from '@/Component/Login/SingIn'
+// import SingIn2 from '@/Component/Login/SingIn2'
+// import LogIn1 from '@/Component/LogIn/LogIn1'
+// import SingIn3 from '@/Component/SingIn/SingIn3'
+// import AboutUsDiscription from "@/Component/AboutUs/AboutUsDiscription";
+// import DetailArticle from "@/Component/Article/DetailArticle";
+// import Doctor from "@/Component/Doctors/Doctors";
+// import FishPeyment from "@/Component/Reservation/FishPeyment";
+// import Information from "@/Component/Reservation/Information";
+// import Payment from "@/Component/Reservation/Payment";
+// import SelectService from "@/Component/Reservation/SelectService";
+// import Service from "@/Component/Reservation/Service";
+// import SetTimeAndDate from "@/Component/Reservation/SetTimeAndDate";
+// import ShowService from "@/Component/Service/ShowService";
+// import ChangePasswordModal from "@/Component/UserDashboard/component/ChangePasswordModal";
+// import MyTurnComponent from "@/Component/UserDashboard/component/MyTurnComponent";
+// import MyComment from "@/Component/UserDashboard/MyComment";
+// import MyTurn from "@/Component/UserDashboard/MyTurn";
+// import Profile from "@/Component/UserDashboard/Profile";
 
 import React, { Fragment } from "react";
 import SampleWork from '@/Component/Landing/SampleWork'
@@ -36,7 +36,7 @@ import UserComment from '@/Component/Landing/UserComent/UserComment'
 function page() {
   return (
     <Fragment >
-      {/* <Navbar/>
+      <Navbar/>
       <ReservationServicePart/>
       <AboutMe/>
       <Services/>
@@ -47,7 +47,7 @@ function page() {
       <ContactWithCustomer/>
       <SampleWork/>
       <UserComment/>
-      <Footer/> */}
+      <Footer/>
 
       {/* <SingIn/> */}
       {/* <SingIn2/> */}
@@ -63,7 +63,7 @@ function page() {
 
       {/* <Doctor/> */}
 
-      <ShowService/>
+      {/* <ShowService/> */}
       {/* 
       <AboutUsDiscription/> */}
 
