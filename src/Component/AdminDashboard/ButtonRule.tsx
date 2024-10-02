88349eda28e4b7d0fa5828d5b5cb31d73c1d3ab4
developersamari <samsa8784@gmail.com>
2024-10-02 17:21:43 +0330

    add Button Rule in admin dashboard

--- src/Component/AdminDashboard/ButtonRule.tsx
+++ src/Component/AdminDashboard/ButtonRule.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const ButtonRule = () => {
   return (
     <>
-      <button className="border-[#DC1E7A] border-solid border-2 rounded-md text-[#DC1E7A] px-4 py-1">لیست نقش ها </button>
+      <button className="border-[#DC1E7A] border-solid border-2 rounded-md text-[#DC1E7A] px-4 py-1">لیست نقش  ها </button>
     </>
   );
 };
