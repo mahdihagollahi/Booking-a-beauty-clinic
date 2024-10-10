c8b46dbbd5afe7de42213ebb687b850c16422579
mahdihagollahi <mahdihaghollahi@gmail.com>
2024-10-10 18:44:17 +0330

    aad Delete user modal in User page at admin panel

--- src/Component/AdminDashboard/Users/ModalDeleteUser.tsx
+++ src/Component/AdminDashboard/Users/ModalDeleteUser.tsx
@@ -1,11 +1,29 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
-const ModalDeleteUser = () => {
+const ModalDeleteUser = ({ handleCloseModal }) => {
   return (
     <Fragment>
+      <div className="flex justify-center mt-64">
+        <div className="bg-white flex flex-col px-10  w-72 h-[362px] rounded-xl py-10">
+          <p className="font-medium text-base mt-20 whitespace-nowrap text-[#292C2C]">
+            آیا برای حذف کاربر اطمینان دارد؟
+          </p>
+          <div className=" flex gap-5 mt-10">
+            <button
+              className="bg-white flex items-center justify-center text-[#DC1E7A] border border-[#DC1E7A] px-4 py-[9px] w-[98px] h-8 rounded-md "
+              onClick={handleCloseModal}
+            >
+              لغو
+            </button>
 
+            <button className="bg-[#DC1E7A] flex items-center justify-center px-4 py-[9px] w-[98px] h-8 rounded-md text-white">
+              تائید
+            </button>
+          </div>
+        </div>
+      </div>
     </Fragment>
-  )
-}
+  );
+};
 
-export default ModalDeleteUser
+export default ModalDeleteUser;
