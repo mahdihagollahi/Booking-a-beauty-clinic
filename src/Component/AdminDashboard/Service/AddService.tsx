c55fa36a2c97850b23ec090075dc3386dedbe38e
mahdihagollahi <mahdihaghollahi@gmail.com>
2024-10-11 18:08:07 +0330

    add service in admin Dashboard

--- src/Component/AdminDashboard/Service/AddService.tsx
+++ src/Component/AdminDashboard/Service/AddService.tsx
@@ -1,67 +1,89 @@
 "use client";
 
-import React, { Fragment, useRef, useState } from "react";
+import React, { Fragment } from "react";
 import WelcomePanel from "@/Component/AdminDashboard/Dashboard/HeaderDashboard";
 import NavbarDashboard from "@/Component/AdminDashboard/Navbar/NavbarDashboard";
-import AddItems from "@/assent/Img/AdminDashboard/AddItems.svg";
 import Image from "next/image";
-
-const AddService = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [error, setError] = useState("");
-  const fileInputRef = useRef(null);
-  const handleImageClick = () => {
-    fileInputRef.current.click();
-  };
-
-  const handleFileChange = (event) => {
-    const file = event.target.files[0];
-    if (file) {
-      setSelectedImage(URL.createObjectURL(file));
-    }
-  };
-
+import AddPhoto from "./AddPhoto";
+import RuleImage from "@/assent/Img/AdminDashboard/RuleAddPhoto.svg";
+const AddService: React.FC = () => {
   return (
     <Fragment>
       <WelcomePanel />
-
       <div>
         <NavbarDashboard />
       </div>
 
       <div className="w-[73.5%] h-[100%] shadow rounded-2xl py-6 px-4 mr-80 -mt-[57%] bg-white ml-72">
         <p className="font-medium text-xl text-[#393939]">خدمات</p>
-        <p className="font-medium text-lg text-[#393939] mt-6">
-          بارگذاری تصویر :
-        </p>
-        <div className="bg-[#FFF2F8] w-[457px] h-[184px] mt-2">
-          <div className="pt-10 pr-40">
-            <input
-              type="file"
-              ref={fileInputRef}
-              style={{ display: "none" }}
-              onChange={handleFileChange}
-              accept="image/*"
-            />
-            {selectedImage ? (
-              <img
-                src={selectedImage}
-                alt="Selected"
-                width={200}
-                height={100}
-              />
-            ) : (
-              <Image
-                src={AddItems}
-                width={100}
-                height={100}
-                alt="Placeholder"
-                onClick={handleImageClick}
-                className="cursor-pointer"
-              />
-            )}
+        <div className="flex gap-[40%]">
+          <div>
+            <p className="font-medium text-lg text-[#393939] mt-6">
+              بارگذاری تصویر :
+            </p>
+            <AddPhoto />
+          </div>
+          <div className="mt-4">
+            <Image src={RuleImage} width={159} height={122} alt="" />
           </div>
         </div>
+        <div className="flex items-center justify-between mt-2">
+          <div>
+            <p className=" mr-2 text-[#2B2B2B] font-medium text-base">
+              نوع خدمات :
+            </p>
+            <input
+              type="text"
+              className="w-[457px] p-4 mb-4 border border-gray-300 rounded-lg mt-1"
+            />
+          </div>
+          <div>
+            <p className=" mr-2 text-[#2B2B2B] font-medium text-base">
+              دسته بندی :
+            </p>
+            <input
+              type="text"
+              className="w-[457px] p-4 mb-4 border border-gray-300 rounded-lg mt-1"
+            />
+          </div>
+        </div>
+
+        <p className=" mr-2 text-[#2B2B2B] font-medium text-base">
+        مقدمه خدمات   :
+        </p>
+        <input
+         
+          className="w-[1032px] h-[172px] p-4 mb-4 border border-gray-300 rounded-lg mt-1"
+        />
+
+
+        <div className="flex items-center justify-between mt-1">
+          <div>
+            <p className=" mr-2 text-[#2B2B2B] font-medium text-base">
+              تعداد جلسات :
+            </p>
+            <input
+              type="text"
+              className="w-[457px] p-4 mb-4 border border-gray-300 rounded-lg "
+            />
+          </div>
+          <div>
+            <p className=" mr-2 text-[#2B2B2B] font-medium text-base">
+              قیمت :
+            </p>
+            <input
+              type="text"
+              className="w-[457px] p-4 mb-4 border border-gray-300 rounded-lg "
+            />
+          </div>
+        </div>
+        <p className=" mr-2 text-[#2B2B2B] font-medium text-base">
+          رده سنی :
+        </p>
+        <input
+          type="text"
+          className="w-[457px] p-4 mb-4 border border-gray-300 rounded-lg mt-1"
+        />
       </div>
     </Fragment>
   );
