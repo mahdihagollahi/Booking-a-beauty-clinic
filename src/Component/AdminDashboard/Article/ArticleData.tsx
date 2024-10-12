b4e59ab46e3c7f6e5da7976115732d8b873787ab
mahdihagollahi <mahdihaghollahi@gmail.com>
2024-10-12 22:32:05 +0330

    add ArticlePage in admin page

---
+++ src/Component/AdminDashboard/Article/ArticleData.tsx
@@ -0,0 +1,96 @@
+import ImageService from "@/assent/Img/Service/Sevices.jpeg";
+const ServiceData = () => {
+  return {
+    Article: [
+      {
+        id: 1,
+        Article: "تزریق ژل لب عروسکی",
+        src: ImageService,
+        Date: "1403/02/03",
+        Detail: "لورم ایپسوم متن ساختگی...",
+      },
+
+      {
+        id: 2,
+        Article: "تزریق ژل لب عروسکی",
+        src: ImageService,
+        Date: "1403/02/03",
+        Detail: "لورم ایپسوم متن ساختگی...",
+      },
+
+      {
+        id: 3,
+        Article: "تزریق ژل لب عروسکی",
+        src: ImageService,
+        Date: "1403/02/03",
+        Detail: "لورم ایپسوم متن ساختگی...",
+      },
+
+      {
+        id: 4,
+        Article: "تزریق ژل لب عروسکی",
+        src: ImageService,
+        Date: "1403/02/03",
+        Detail: "لورم ایپسوم متن ساختگی...",
+      },
+
+      {
+        id: 5,
+        Article: "تزریق ژل لب عروسکی",
+        src: ImageService,
+        Date: "1403/02/03",
+        Detail: "لورم ایپسوم متن ساختگی...",
+      },
+
+      {
+        id: 6,
+        Article: "تزریق ژل لب عروسکی",
+        src: ImageService,
+        Date: "1403/02/03",
+        Detail: "لورم ایپسوم متن ساختگی...",
+      },
+
+      {
+        id: 7,
+        Article: "تزریق ژل لب عروسکی",
+        src: ImageService,
+        Date: "1403/02/03",
+        Detail: "لورم ایپسوم متن ساختگی...",
+      },
+
+      {
+        id: 8,
+        Article: "تزریق ژل لب عروسکی",
+        src: ImageService,
+        Date: "1403/02/03",
+        Detail: "لورم ایپسوم متن ساختگی...",
+      },
+
+      {
+        id: 9,
+        Article: "تزریق ژل لب عروسکی",
+        src: ImageService,
+        Date: "1403/02/03",
+        Detail: "لورم ایپسوم متن ساختگی...",
+      },
+
+      {
+        id: 10,
+        Article: "تزریق ژل لب عروسکی",
+        src: ImageService,
+        Date: "1403/02/03",
+        Detail: "لورم ایپسوم متن ساختگی...",
+      },
+
+      {
+        id: 11,
+        Article: "تزریق ژل لب عروسکی",
+        src: ImageService,
+        Date: "1403/02/03",
+        Detail: "لورم ایپسوم متن ساختگی...",
+      },
+    ],
+  };
+};
+
+export default ServiceData;
